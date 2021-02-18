af16b9e618c2cea23c98c597d97784134ba088ec
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-17 21:43:13 -0600

    Initialization (not working)

---
+++ FirstComeFirstServedTickets/FirstComeFirstServedTickets/Program.cs
@@ -0,0 +1,68 @@
+﻿/**************************************************************
+* Name        : FirstComeFirstServedTickets
+* Author      : Kabrina Brady
+* Created     : 2/17/2021
+* Course      : Data Structures
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe
+* Academic Honesty: I attest that this is my original work. I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+
+using System;
+using System.Collections;
+
+namespace FirstComeFirstServedTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            Random randomGenerator = new Random();
+            int totalTickets = 8000;
+            int lineSize = randomGenerator.Next(1, 1000);
+            int[] numCustomers = new int[lineSize];
+            int numCustomersWhoPurchased = 0;
+
+            foreach(int cust in numCustomers)
+            {
+                q.Enqueue(cust);
+            }
+
+            foreach(int cust in q)
+            {
+                int numTicketsPurchased = randomGenerator.Next(1, 4);
+                Console.WriteLine("Customer wants to purchase " + numTicketsPurchased + " tickets." + "\n");
+
+                if(totalTickets >= numTicketsPurchased)
+                {
+                    totalTickets -= numTicketsPurchased;
+                    numCustomersWhoPurchased++;
+
+                    if(q.Count > 0)
+                    {
+                        q.Dequeue();
+                        Console.WriteLine("Happy customers (who were able to purchase tickets): " + numCustomersWhoPurchased);
+                        Console.WriteLine("Tickets remaining: " + totalTickets + "\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("There were more tickets than customers. Number of customers: " + numCustomersWhoPurchased + ", Number of remaining tickets: " + totalTickets);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Customer tried to purchase more tickets than there are available. Customer wanted " + numTicketsPurchased + " tickets, but there were only " + totalTickets + " tickets remaining.");
+                    break;
+                }
+            }
+        }
+    }
+}
